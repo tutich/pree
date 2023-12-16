5e48c25e2ddf2c2458b892f4801a1c3ead437566
tutich <pkoinange02@gmail.com>
2023-12-17 02:58:17 +0300

    nodemon

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-const express = require("express");
+const express = require('express');
+const app = express()
+const corse = require('corse')
+
+app.use(corse())
+
+app.get('/hello', (req, res) => {
+    res.send('hello world')
+})
+
+app.listen(1300, () => {
+    console.log('server started on 1300')
+})
