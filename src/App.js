f83570c0602da5d21f007026b4a105757f14db78
tutich <pkoinange02@gmail.com>
2023-12-18 23:58:19 +0300

    jwt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+const App = () => {
+	return (
+		<div>
+			<BrowserRouter>
+				<Route path="/login" exact component={Login} />
+				<Route path="/register" exact component={Register} />
+				<Route path="/dashboard" exact component={Dashboard} />
+			</BrowserRouter>
+		</div>
+	)
 }
 
-export default App;
+export default App
